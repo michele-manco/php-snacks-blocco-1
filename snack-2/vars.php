0bef31e2dc8aa7e91f51826af414c9883a94621e
Michele_Manco <motlamik@gmail.com>
2020-01-23 20:22:10 +0100

    mod

--- snack-2/vars.php
+++ snack-2/vars.php
@@ -13,8 +13,13 @@
 // echo $nome;  }
 // }
 //
+$name = strlen($nome);
+$pos_punto = strpos($email, '.');
+$pos_chiocciola = strpos($email, '@');
+$sage = intval($age);
+
 function verify($name, $email, $age){
-  if ($name > 3 && $pos_punto !== false && $pos_chiocciola !== false && !is_numeric ($age)) {
+  if ($name > 3 && $pos_punto !== false && $pos_chiocciola !== false && is_numeric ($age)) {
     echo "access granted";
   } else {
     echo "access denied";
